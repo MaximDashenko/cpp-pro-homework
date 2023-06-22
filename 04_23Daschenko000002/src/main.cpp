71add68f52ba16612e4d5edbf682f030e219e659
Maxim Dashenko <maximdashenko15@gmail.com>
2023-06-22 13:39:42 +0300

    04_23Daschenko000002

--- 04_23Daschenko000002/src/main.cpp
+++ 04_23Daschenko000002/src/main.cpp
@@ -11,95 +11,88 @@ private:
     std::string groupNumber;
 
 public:
-
     Student(std::string name, std::string dateOfBirth, std::string phoneNumber,
             std::string address, std::string groupNumber)
-            : name(std::move(name)), dateOfBirth(std::move(dateOfBirth)), phoneNumber(std::move(phoneNumber)), address(std::move(address)),
-              groupNumber(std::move(groupNumber)) {}
-
+            : name(std::move(name)), dateOfBirth(std::move(dateOfBirth)), phoneNumber(std::move(phoneNumber)),
+              address(std::move(address)), groupNumber(std::move(groupNumber)) {}
 
     void enterData() {
-        std::cout << "Введіть ім'я: ";
+        std::cout << "Enter name: ";
         std::getline(std::cin >> std::ws, name);
 
-        std::cout << "Введіть дату народження: ";
+        std::cout << "Enter date of birth: ";
         std::getline(std::cin >> std::ws, dateOfBirth);
 
-        std::cout << "Введіть номер телефону: ";
+        std::cout << "Enter phone number: ";
         std::getline(std::cin >> std::ws, phoneNumber);
 
-        std::cout << "Введіть адресу: ";
+        std::cout << "Enter address: ";
         std::getline(std::cin >> std::ws, address);
 
-        std::cout << "Введіть номер навчальної групи: ";
+        std::cout << "Enter group number: ";
         std::getline(std::cin >> std::ws, groupNumber);
     }
 
-
     void displayData() const {
-        std::cout << "Ім'я: " << name << std::endl;
-        std::cout << "Дата народження: " << dateOfBirth << std::endl;
-        std::cout << "Номер телефону: " << phoneNumber << std::endl;
-        std::cout << "Адреса: " << address << std::endl;
-        std::cout << "Номер навчальної групи: " << groupNumber << std::endl;
+        std::cout << "Name: " << name << std::endl;
+        std::cout << "Date of Birth: " << dateOfBirth << std::endl;
+        std::cout << "Phone Number: " << phoneNumber << std::endl;
+        std::cout << "Address: " << address << std::endl;
+        std::cout << "Group Number: " << groupNumber << std::endl;
     }
 
     void editData() {
-        std::cout << "Виберіть поле для редагування:" << std::endl;
-        std::cout << "1. Ім'я" << std::endl;
-        std::cout << "2. Дата народження" << std::endl;
-        std::cout << "3. Номер телефону" << std::endl;
-        std::cout << "4. Адреса" << std::endl;
-        std::cout << "5. Номер навчальної групи" << std::endl;
+        std::cout << "Select a field to edit:" << std::endl;
+        std::cout << "1. Name" << std::endl;
+        std::cout << "2. Date of Birth" << std::endl;
+        std::cout << "3. Phone Number" << std::endl;
+        std::cout << "4. Address" << std::endl;
+        std::cout << "5. Group Number" << std::endl;
 
         int choice;
         std::cin >> choice;
 
         switch (choice) {
             case 1:
-                std::cout << "Введіть нове ім'я: ";
+                std::cout << "Enter new name: ";
                 std::getline(std::cin >> std::ws, name);
                 break;
             case 2:
-                std::cout << "Введіть нову дату народження: ";
+                std::cout << "Enter new date of birth: ";
                 std::getline(std::cin >> std::ws, dateOfBirth);
                 break;
             case 3:
-                std::cout << "Введіть новий номер телефону: ";
+                std::cout << "Enter new phone number: ";
                 std::getline(std::cin >> std::ws, phoneNumber);
                 break;
             case 4:
-                std::cout << "Введіть нову адресу: ";
+                std::cout << "Enter new address: ";
                 std::getline(std::cin >> std::ws, address);
                 break;
             case 5:
-                std::cout << "Введіть новий номер навчальної групи: ";
+                std::cout << "Enter new group number: ";
                 std::getline(std::cin >> std::ws, groupNumber);
                 break;
             default:
-                std::cout << "Неправильний вибір!" << std::endl;
+                std::cout << "Invalid choice!" << std::endl;
         }
     }
 };
 
 int main() {
-    Student student("Ім'я студента", "01.01.2000", "123456789", "Адреса студента", "Група 1");
-
+    Student student("Student Name", "01.01.2000", "123456789", "Student Address", "Group 1");
 
     student.enterData();
 
     std::cout << std::endl;
 
-
     student.displayData();
 
     std::cout << std::endl;
 
-
     student.editData();
 
     std::cout << std::endl;
-
 
     student.displayData();
 
